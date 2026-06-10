6253ea4ff52f7f6aa3372567089912b07111eeaa
romesilcik-hub <romesilcik@gmail.com>
2026-06-10 15:44:33 +0200

    Add files via upload

--- aj_dril/sw.js
+++ aj_dril/sw.js
@@ -1,6 +1,6 @@
 // AJ Dril — Service Worker
 // Verze cache — změň číslo když aktualizuješ aplikaci
-const CACHE_NAME = 'aj-dril-v17';
+const CACHE_NAME = 'aj-dril-v18';
 
 const ASSETS = [
   './',
